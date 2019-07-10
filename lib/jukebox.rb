5fced40770b4fc9cd4bb0a01c4b869056d3c12ad
Gianfranco Nuschese <gianfranconuschese@gmail.com>
2019-07-10 23:42:55 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -42,14 +42,18 @@ end
 
 def run(songs)
   help
-  
-  puts "Please enter a command:"
-  user_command = gets.chomp
-  if user_command == "play"
-    play(songs)
-  elsif user_command == "help"
-    help
-  elsif user_command == "list"
-    list(songs)
+  while(true)
+    puts "Please enter a command:"
+    user_command = gets.chomp
+    if user_command == "play"
+      play(songs)
+    elsif user_command == "help"
+      help
+    elsif user_command == "list"
+      list(songs)
+    elsif user_command == "exit"
+      break
+    end
   end
+  exit_jukebox
 end
